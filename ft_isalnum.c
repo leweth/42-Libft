1499c7504b952383c010d168189325f91142d259
Med Walid <96090291+leweth@users.noreply.github.com>
2024-01-22 09:36:30 +0100

    Update ft_isalnum.c
    Added explanation.

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,6 +12,14 @@
 
 #include "libft.h"
 
+/*
+	DESCRIPTION :
+	The function ft_isalnum checks whether the value of c is alphanumeric.
+
+	RETURN VALUE :
+	Non-zero if c is alphanumeric, zero if not.
+*/
+
 int	ft_isalnum(int c)
 {
 	if (ft_isalpha(c) || ft_isdigit(c))
