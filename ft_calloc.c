74078bedbb86086ceec5be62d9fcde202305e511
Med Walid <96090291+leweth@users.noreply.github.com>
2024-01-22 09:34:47 +0100

    Update ft_calloc.c
    Added explanation.

--- ft_calloc.c
+++ ft_calloc.c
@@ -12,6 +12,15 @@
 
 #include "libft.h"
 
+/*
+	DESCRIPTION :
+	The function ft_calloc allocates memory for an array of count elements
+	of size bytes each and sets the memory to zero.
+
+	RETURN VALUE :
+	The pointer to the allocated memory. NULL if the memory allocation fails.
+*/
+
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
